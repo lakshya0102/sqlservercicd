fe80c9bb80aa5a14ee36b0d9a2813bb715d6902c
lakshya0102 <srivastavalakshya1@gmail.com>
2023-03-14 21:03:36 +0530

    "update"

---
+++ dbo/Tables/Table_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table_1] (
+    [student]  NCHAR (10) NOT NULL,
+    [employee] NCHAR (10) NULL,
+    [address]  NCHAR (10) NULL
+);
+
---
+++ dbo/Tables/lakshya.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[lakshya] (
+    [lakshya] NCHAR (10) NULL,
+    [shivam]  NCHAR (10) NULL,
+    [manush]  NCHAR (10) NULL,
+    [chirag]  NCHAR (10) NULL
+);
+
